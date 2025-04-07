a472ca66a559bb78f36994a0de582b6c64788592
naren-ngk <narensakthiit@gmail.com>
2025-04-07 19:30:18 +0530

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { WixClientContextProvider } from "@/context/wixContext";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <WixClientContextProvider>
           <Navbar />
-          {children}
+          <Suspense fallback={<div>Loading...</div>}>
+            {children}
+          </Suspense>
           <Footer />
         </WixClientContextProvider>
       </body>

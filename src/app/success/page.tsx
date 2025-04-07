fcbf64586abab7d69db6bc64dc1c3d4e02fdd3c5
naren-ngk <narensakthiit@gmail.com>
2025-04-07 19:25:06 +0530

    byg fix

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -22,7 +22,7 @@ const SuccessPage = () => {
     }, [router, orderId]);
 
     return (
-        <Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
             <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
                 <Confetti width={2000} height={1000} />
                 <h1 className="text-6xl text-green-700">Successful</h1>

049edf8f29b70f511d3534e17c02c182865a53ed
Narendran G <narendran@Narens-MacBook.local>
2025-07-07 14:13:48 +0530

    layout ui fix

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -850,7 +850,7 @@ const ProductsWithFilters = ({ products, filters, name, searchParam, isMobile }:
               </form>
 
               {/* Product grid */}
-              <div className="lg:col-span-3 flex md:gap-x-8 gap-y-16 justify-center items-center flex-wrap">
+              <div className="lg:col-span-3 flex md:gap-x-8 md:gap-y-16 justify-center items-center flex-wrap">
                 <AllProducts products={filteredProducts} searchParams={searchParam} isMobile={isMobile} />
               </div>
             </div>
